d1c16111c0bfb0be41cbe4f73cc4aad0ea355426
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-17 15:26:31 -0700

    complete edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,22 +2,15 @@ const request = require('request');
 const args = process.argv.slice(2);
 
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  // console.log("type of body:", typeof body);
-  const data = JSON.parse(body);
-  // console.log("data:", data);
-  // console.log("type of data:", typeof data);
-
-  for (key in data) {
-    console.log("description:", data[key].description);
+request(`https://api.thecatapi.con/v1/breeds/search?q=${args}`, (error, response, body) => {
+  if (error) {
+    return console.log(error);
+  }
+  const [data] = JSON.parse(body);
+  if (data) {
+    console.log("description:", data.description);
+  }
+  else {
+    console.log("This breed does not exist.");
   }
 });
-
-
-//Endpoint that will return cat breed results (empty array):
-// https://api.thecatapi.com/v1/breeds/search
-
-
-
-//For Siberian (array with results):
-//https://api.thecatapi.com/v1/breeds/search?q=sib 
