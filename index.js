9c95e57ec76c48e6ead702a6ca0251f8d73e2c4f
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-17 16:52:24 -0700

    refactor

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, description) => {
   if (error) {
-    console.log('Error fetch details:', error);
+    console.log('Error details:', error);
   } else {
     console.log('Description: ', description);
   }
