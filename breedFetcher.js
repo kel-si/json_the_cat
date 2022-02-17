5ed6ee849e8babdf2232605e62fd6c91d68c5507
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-17 14:54:43 -0700

    log a description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,15 @@
 const request = require('request');
+const args = process.argv.slice(2);
 
 
 request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  console.log("type of body:", typeof body);
+  // console.log("type of body:", typeof body);
   const data = JSON.parse(body);
-  console.log("data:", data);
-  console.log("type of data:", typeof data);
+  // console.log("data:", data);
+  // console.log("type of data:", typeof data);
 
   for (key in data) {
-    console.log("data[key]:", data[key]);
+    console.log("description:", data[key].description);
   }
 });
 
